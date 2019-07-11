ff3359e063fb1122448caf4dfec805ade798d2b9
Tommaso Leonardi <tom@itm6.xyz>
2019-07-11 18:11:18 +0200

    Formal fix

--- ncRNAs_structures/7SK/generate_figures.sh
+++ ncRNAs_structures/7SK/generate_figures.sh
@@ -9,10 +9,10 @@ thr="0.01"
 pval_col=8
 real_start=52995620
 
-r2r  --GSC-weighted-consensus RF00100.stockholm.txt $template 0.97 0.9 0.75 4 0.97 0.9 0.75 0.5 0.1
-cat <(head -n -1 7sk.sto) <(7sk_custom_annots.sto) <(echo //) > 7sk_consensus_structure.sto
-cat <(head -n -1 7sk.sto) <(7sk_custom_annots.sto) <(python create_annotations.py $nanocomp_res_kd $nanocomp_bed $template $tx 0.01 $pval_col $real_start) <(echo //) > 7sk_mettl3.sto
-cat <(head -n -1 7sk.sto) <(7sk_custom_annots.sto) <(python create_annotations.py $nanocomp_res_ivt $nanocomp_bed $template $tx 0.01 $pval_col $real_start) <(echo //) > 7sk_ivt.sto
-cat <(head -n -1 7sk.sto) <(7sk_custom_annots.sto) <(python create_annotations.py $nanocomp_res_dkc $nanocomp_bed $template $tx 0.1 $pval_col $real_start) <(echo //) > 7sk_dkc1.sto
+r2r  --GSC-weighted-consensus RF00100.stockholm.txt $template 3 0.97 0.9 0.75 4 0.97 0.9 0.75 0.5 0.1
+cat <(head -n -1 $template) 7sk_custom_annots.txt <(echo //) > 7sk_consensus_structure.sto
+cat <(head -n -1 $template) 7sk_custom_annots.txt <(python create_annotations.py $nanocomp_res_kd $nanocomp_bed $template $tx 0.01 $pval_col $real_start) <(echo //) > 7sk_mettl3.sto
+cat <(head -n -1 $template) 7sk_custom_annots.txt <(python create_annotations.py $nanocomp_res_ivt $nanocomp_bed $template $tx 0.01 $pval_col $real_start) <(echo //) > 7sk_ivt.sto
+cat <(head -n -1 $template) 7sk_custom_annots.txt <(python create_annotations.py $nanocomp_res_dkc $nanocomp_bed $template $tx 0.1 $pval_col $real_start) <(echo //) > 7sk_dkc1.sto
 
 r2r 7sk.meta 7sk.pdf
