d2c28e39fc204b2f30f84b03190c15782ffac8e5
Tommaso Leonardi <tom@itm6.xyz>
2019-07-13 10:47:56 +0200

    Switched to rc3-1 for METTL3DK_ncRNAs

--- ncRNAs_structures/7SK/generate_figures.sh
+++ ncRNAs_structures/7SK/generate_figures.sh
@@ -5,7 +5,8 @@
 
 BASEDIR=$(git rev-parse --show-toplevel)
 WD="${BASEDIR}/ncRNAs_structures/7SK/"
-nanocomp_res_kd="${BASEDIR}/7sk/data/KD/nanocompore/out_nanocompore_results.tsv"
+nanocomp_res_kd="${BASEDIR}/nanocompore_pipelines/METTL3_KD_ncRNAs/results/nanocompore/out_nanocompore_results.tsv"
+#nanocomp_res_kd="${BASEDIR}/7sk/data/KD/nanocompore/out_nanocompore_results.tsv"
 nanocomp_res_ivt="${BASEDIR}/nanocompore_pipelines/IVT_7SK/results/nanocompore/out_nanocompore_results.tsv"
 #nanocomp_res_dkc="${BASEDIR}/nanocompore_pipelines/DKC1_ncRNAs/results/nanocompore/out_nanocompore_results.tsv"
 nanocomp_res_dkc="${BASEDIR}/7sk/data/DKC1/nanocompore/out_nanocompore_results.tsv"
@@ -19,7 +20,9 @@ real_start=52995620
 r2r  --GSC-weighted-consensus ${WD}/RF00100.stockholm.txt $template 3 0.97 0.9 0.75 4 0.97 0.9 0.75 0.5 0.1
 cat <(head -n -1 $template) ${WD}/7sk_custom_annots.txt <(echo //) > ${WD}/7sk_consensus_structure.sto
 cat <(head -n -1 $template) ${WD}/7sk_custom_annots.txt <(python ${WD}/create_annotations.py $nanocomp_res_kd $nanocomp_bed $template $tx 0.01 8 $real_start) <(echo //) > ${WD}/7sk_mettl3.sto
+
 cat <(head -n -1 $template) ${WD}/7sk_custom_annots.txt <(python ${WD}/create_annotations.py $nanocomp_res_ivt $nanocomp_bed $template $tx 0.01 6 $real_start) <(echo //) > ${WD}/7sk_ivt.sto
+
 cat <(head -n -1 $template) ${WD}/7sk_custom_annots.txt <(python ${WD}/create_annotations.py $nanocomp_res_dkc $nanocomp_bed $template $tx 0.1 8 $real_start) <(echo //) > ${WD}/7sk_dkc1.sto
 
 r2r ${WD}/7sk.meta ${WD}/7sk.pdf
