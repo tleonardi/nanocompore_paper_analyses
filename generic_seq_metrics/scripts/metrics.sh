0f111d0e5ca95fb1e529cfb8006ca8cc70f922e8
Tommaso Leonardi <tl344@ebi.ac.uk>
2019-08-06 11:31:19 +0100

    Added generic_seq_metrics for TRM5_KO

--- generic_seq_metrics/scripts/metrics.sh
+++ generic_seq_metrics/scripts/metrics.sh
@@ -2,22 +2,18 @@
 BASEDIR=$(git rev-parse --show-toplevel)
 
 
-mkdir -p $BASEDIR/generic_seq_metrics/results/METTL3_KD_polyA
-(
-echo -ne "WT_1\t"
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/WT_1/minimap.filt.sort.bam | cut -f1 | sort -u | wc -l
-echo -ne "WT_2\t"
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/WT_2/minimap.filt.sort.bam | cut -f1 | sort -u | wc -l
-echo -ne "KD_1\t"
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/KD_1/minimap.filt.sort.bam | cut -f1 | sort -u | wc -l
-echo -ne "KD_2\t"
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/KD_2/minimap.filt.sort.bam | cut -f1 | sort -u | wc -l
-)> $BASEDIR/generic_seq_metrics/results/METTL3_KD_polyA/n_mapped_reads.txt
+DATASETS="METTL3_KD_polyA TRM5_KO_polyA"
+for dataset in $DATASETS; do
+	mkdir -p $BASEDIR/generic_seq_metrics/results/$dataset
+	(for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
+	echo -ne "$(basename $(dirname $f))\t"
+	samtools view $f | cut -f1 | sort -u | wc -l
+	done
+	)> $BASEDIR/generic_seq_metrics/results/$dataset/n_mapped_reads.txt
+	
+	(for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
+	SAMPLE=$(basename $(dirname $f))
+	samtools view $f | cut -f3 | sort | uniq -c | awk -v samp=$SAMPLE'BEGIN{OFS="\t"}{print samp, $1,$2}' 
+	)> $BASEDIR/generic_seq_metrics/results/$dataset/tx_counts.txt
 
-
-(
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/WT_1/minimap.filt.sort.bam | cut -f3 | sort | uniq -c | awk 'BEGIN{OFS="\t"}{print "WT_1", $1,$2}' 
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/WT_2/minimap.filt.sort.bam | cut -f3 | sort | uniq -c | awk 'BEGIN{OFS="\t"}{print "WT_2", $1,$2}' 
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/KD_1/minimap.filt.sort.bam | cut -f3 | sort | uniq -c | awk 'BEGIN{OFS="\t"}{print "KD_1", $1,$2}' 
-samtools view $BASEDIR/nanocompore_pipelines/METTL3_KD_polyA/results/KD_2/minimap.filt.sort.bam | cut -f3 | sort | uniq -c | awk 'BEGIN{OFS="\t"}{print "KD_2", $1,$2}' 
-)> $BASEDIR/generic_seq_metrics/results/METTL3_KD_polyA/tx_counts.txt
+done
