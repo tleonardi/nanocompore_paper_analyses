f66c5867d5ae201f264cd5efc06e1affc6305f8a
Tommaso Leonardi <tom@itm6.xyz>
2019-09-11 14:49:14 +0200

    Fixed syntax error

--- generic_seq_metrics/scripts/metrics.sh
+++ generic_seq_metrics/scripts/metrics.sh
@@ -5,15 +5,18 @@ BASEDIR=$(git rev-parse --show-toplevel)
 DATASETS="METTL3_KD_polyA TRM5_KO_polyA"
 for dataset in $DATASETS; do
 	mkdir -p $BASEDIR/generic_seq_metrics/results/$dataset
-	(for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
+	(
+	for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
 	echo -ne "$(basename $(dirname $f))\t"
 	samtools view $f | cut -f1 | sort -u | wc -l
 	done
 	)> $BASEDIR/generic_seq_metrics/results/$dataset/n_mapped_reads.txt
 	
-	(for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
+	(
+	for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
 	SAMPLE=$(basename $(dirname $f))
-	samtools view $f | cut -f3 | sort | uniq -c | awk -v samp=$SAMPLE'BEGIN{OFS="\t"}{print samp, $1,$2}' 
+	samtools view $f | cut -f3 | sort | uniq -c | awk -v samp=$SAMPLE 'BEGIN{OFS="\t"}{print samp, $1,$2}' 
+ 	done
 	)> $BASEDIR/generic_seq_metrics/results/$dataset/tx_counts.txt
 
 done
