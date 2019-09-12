63bd136e307f7a7d2ee6d1a08a4027631d9a4dbd
Tommaso Leonardi <tl344@ebi.ac.uk>
2019-09-12 15:47:57 +0100

    Added DKC1_kd generic metrics

--- generic_seq_metrics/scripts/metrics.sh
+++ generic_seq_metrics/scripts/metrics.sh
@@ -20,3 +20,12 @@ for dataset in $DATASETS; do
 	)> $BASEDIR/generic_seq_metrics/results/$dataset/tx_counts.txt
 
 done
+
+dataset="DKC1_ncRNAs"
+mkdir -p $BASEDIR/generic_seq_metrics/results/$dataset
+(
+for f in $BASEDIR/nanocompore_pipelines/$dataset/results/*/minimap.filt.sort.bam; do
+SAMPLE=$(basename $(dirname $f))
+samtools view $f | cut -f3 | sort | uniq -c | awk -v samp=$SAMPLE 'BEGIN{OFS="\t"}{print samp, $1,$2}' 
+done
+)> $BASEDIR/generic_seq_metrics/results/$dataset/tx_counts.txt
