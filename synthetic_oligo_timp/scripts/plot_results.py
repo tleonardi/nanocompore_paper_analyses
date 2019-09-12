0d068b349923851169b69674cfebdea9f8fd0a66
Tommaso Leonardi <tom@itm6.xyz>
2019-09-12 13:45:25 +0200

    Regenerated synth plots

--- synthetic_oligo_timp/scripts/plot_results.py
+++ synthetic_oligo_timp/scripts/plot_results.py
@@ -14,7 +14,7 @@ p1[0].savefig(dbpath+"/signal_plot.svg")
 p2=s.plot_pvalue(ref_id='FLuc_Control_Plasmid:88-1805', plot_style='seaborn-whitegrid', figsize=[12,8])
 p2[0].savefig(dbpath+"/pvalue_plot.svg")
 
-p3=s.plot_position(ref_id='FLuc_Control_Plasmid:88-1805', pos=4, plot_style='seaborn-whitegrid', figsize=[14,10])
+p3=s.plot_position(ref_id='FLuc_Control_Plasmid:88-1805', pos=4, kde=False, model=True, plot_style='seaborn-whitegrid', figsize=[14,10])
 p3[0].savefig(dbpath+"/pos4_plot.svg")
 
 
