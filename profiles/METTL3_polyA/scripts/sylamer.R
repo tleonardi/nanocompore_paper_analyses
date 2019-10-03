e9f325c5d0626a03f90ff2d4a7348fcee572fad3
Tommaso Leonardi <tom@itm6.xyz>
2019-10-03 11:06:58 +0200

    Updated sylamer plots

--- profiles/METTL3_polyA/scripts/sylamer.R
+++ profiles/METTL3_polyA/scripts/sylamer.R
@@ -10,7 +10,7 @@ scores <- read_delim(paste0(BASEDIR, "/full_sites_ext.fa.ids.txt"), delim="_", c
 score_thr <- (filter(scores, X3>-log10(0.01)) %>% nrow )
 
 top_motifs <- filter(syl, value>10) %>% pull(upper) %>% unique
-top_auc <- group_by(syl, upper) %>% summarise(AUC=sum(value)) %>% top_n(5, AUC) %>% pull(upper)
+top_auc <- group_by(syl, upper) %>% summarise(AUC=sum(value)) %>% top_n(1, AUC) %>% pull(upper)
 top_100_auc <- group_by(syl, upper) %>% summarise(AUC=sum(value)) %>% top_n(100, AUC) %>% pull(upper)
 
 syl <- syl %>% mutate(label=case_when(upper%in%top_auc~upper, T~"Other")) %>% mutate(label=gsub("T", "U", label))
@@ -19,5 +19,5 @@ syl[syl$label=="Other", "label"] <- NA
 #ggplot(syl, aes(x=variable, y=value, group=upper, colour=label)) + geom_line()
 
 pdf(paste0(RESULTS, "/sylamer.pdf"), width=12)
-filter(syl, upper%in%top_100_auc) %>% ggplot(aes(x=variable, y=value, group=upper, colour=label)) + geom_line() +geom_vline(xintercept=score_thr) + theme_classic(20) + xlab("Ranked sequences") + ylab("Hypergeometric p-value\n(-log10)") + labs(colour="Motif")
+filter(syl, upper%in%top_100_auc) %>% ggplot(aes(x=variable, y=value, group=upper, colour=label)) + geom_line(size=0.3) +geom_vline(xintercept=score_thr, size=0.5, linetype=2) + theme_classic(24) + xlab("Ranked sequences") + ylab("Hypergeometric p-value\n(-log10)") + labs(colour="Motif") + theme(axis.line = element_line(colour = 'black', size = 0.5))
 dev.off() 
