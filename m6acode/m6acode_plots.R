86b05a83e0d317fbca1fbd380555fcb35f271bfa
Tommaso Leonardi <tom@itm6.xyz>
2019-11-13 18:57:18 +0100

    Last version of m6A code and analysis

--- m6acode/m6acode_plots.R
+++ m6acode/m6acode_plots.R
@@ -6,7 +6,7 @@ ROOTDIR=system2("git", args=c("rev-parse", "--show-toplevel"), stdout=T)
 BASEDIR=paste0(ROOTDIR, "/m6acode/")
 
 # [1533, 650, 1322] 
-p <- read_tsv(paste0(BASEDIR,"out/out.tsv"), col_names=c("lab", "pos", "read", "p")) %>% mutate(lab=gsub("_[12]", "", lab), pos=paste0("A",pos)) 
+p <- read_tsv(paste0(BASEDIR,"out_actin/out.tsv"), col_names=c("lab", "pos", "read", "p")) %>% mutate(lab=gsub("_[12]", "", lab), pos=paste0("A",pos)) 
 
 wt <- filter(p, lab=="WT") %>%
 	reshape2::dcast(read~pos) %>% 
@@ -22,10 +22,14 @@ kd <- 	filter(p, lab=="KD") %>%
 	ggheatmap::ggheatmap(colorPalette="GnBu", orderCol=F, scaleName="p modified") +
 	ggtitle("KD")
 
-thr=0.5
-intersections <- list(Pos650=filter(p, lab=="WT", pos=="A650", p>thr) %>% pull(read), 
-		      Pos1322=filter(p, lab=="WT", pos=="A1322", p>thr) %>% pull(read), 
-		      Pos1533=filter(p, lab=="WT", pos=="A1533", p>thr) %>% pull(read))
+thr=0.75
+int_tibble <- filter(p, lab=="WT") %>%
+	reshape2::dcast(read~pos) %>% 
+	filter(!is.na(A650), !is.na(A1322), !is.na(A1533)) %>% as_tibble()
+
+intersections <- list(Pos650=filter(int_tibble, A650>thr) %>% pull(read), 
+		      Pos1322=filter(int_tibble, A1322>thr) %>% pull(read), 
+		      Pos1533=filter(int_tibble, A1533>thr) %>% pull(read))
 n=filter(p, lab=="WT", p>thr) %>% pull(read) %>% unique %>% length
 
 #mat <- filter(p, lab=="WT") %>%
@@ -55,20 +59,17 @@ ggplot(p, aes(x=p, fill=lab)) + geom_density(colour="black", alpha=0.4) + facet_
 upset(fromList(intersections), text.scale=1.3)
 
 Result=supertest(intersections,n=n)
-#plot the intersections with a split y-axis
-#show elements of the intersections with no more than 20 elements
-#png('examples/ex1.png',width=2000,height=2000,res=300)
-plot(Result, Layout="landscape", sort.by="size", keep=FALSE,
-	show.elements=FALSE,
-	show.expected.overlap=TRUE,expected.overlap.style="hatchedBox",
-	color.expected.overlap='red')
-
-
-mset2df(Result) %>% ggplot(aes(x=Names, y=Size, fill=-log10(pvalue))) + geom_col(colour="black") + geom_col(aes(y=Expected), colour="red", linetype="dotted") + geom_text(aes(y=Size+60, label=paste0(Size,"\n(",round(Size/n,3)*100,"%)"))) + scale_fill_distiller(palette = "OrRd", direction=1, na.value="white") + theme_bw(22) + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + xlab("")
+mset2df(Result) %>% ggplot(aes(x=Names, y=Size, fill=-log10(pvalue))) + 
+	geom_col(colour="black") + 
+	geom_col(aes(y=Expected), colour="red", linetype="dotted") + 
+	geom_text(aes(y=Size+60, label=paste0(Size,"\n(",round(Size/n,3)*100,"%)"))) + 
+	scale_fill_distiller(palette = "OrRd", direction=1, na.value="white") + 
+	theme_bw(22) + 
+	theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) + xlab("")
 dev.off()
 
 
-dat <- read_tsv(paste0(BASEDIR,"out/out_data.tsv"), col_names=c("pos", "lab", "cluster", "prob", "intensity", "dwell")) %>% mutate(lab=gsub("_[12]", "", lab), pos=paste0("A",pos+2)) %>%
+dat <- read_tsv(paste0(BASEDIR,"out_actin/out_data.tsv"), col_names=c("pos", "lab", "cluster", "prob", "intensity", "dwell")) %>% mutate(lab=gsub("_[12]", "", lab), pos=paste0("A",pos+2)) %>%
 	mutate(pos=factor(pos, levels=c("A652", "A1324", "A1535"))) %>%
 	mutate(lab=factor(lab, levels=c("WT", "KD"))) %>%
 	group_by(pos) %>% 
@@ -82,3 +83,41 @@ ggplot(dat, aes(x=norm_int, y=norm_dwell)) +  geom_point(aes(colour=prob), size=
 	xlab("Current intensity (z-score normalised)") +
 	ylab("Dwell time (log10, z-score normalised)")
 dev.off() 
+
+
+mat <- filter(p, lab=="WT") %>% reshape2::dcast(read~pos) %>%
+        select(-read) %>% filter(!is.na(A650), !is.na(A1322), !is.na(A1533)) %>% as_tibble()
+
+matkd <- filter(p, lab=="KD") %>% reshape2::dcast(read~pos) %>%
+        select(-read) %>% filter(!is.na(A650), !is.na(A1322), !is.na(A1533)) %>% as_tibble()
+
+getprob <- function(x, mod_freqs){
+                if(x[1]) {p1 <- mod_freqs[["A650"]]} else  {p1 <-(1 - mod_freqs[["A650"]])}
+                if(x[2]) {p2 <- mod_freqs[["A1322"]]} else {p2 <-(1 - mod_freqs[["A1322"]])}
+                if(x[3]) {p3 <- mod_freqs[["A1533"]]} else {p3 <-(1 - mod_freqs[["A1533"]])}
+                return(p1*p2*p3)
+}
+
+calc_p <-function(thr, full=F){
+        binpos <- (mat> thr) %>% as_tibble()
+        obs_n <- binpos %>% group_by(A650, A1322, A1533) %>% summarise(n=n())
+        n_tot <- sum(obs_n$n)
+        freq <- apply(binpos, 2, mean)
+        obs_n$exp <- round(n_tot * apply(obs_n, 1, function(x) getprob(x, freq)))
+        obs_n <- mutate(obs_n, chi = ((n-exp)^2)/exp)
+	if(!full) return(pchisq(sum(obs_n$chi), df=7, lower.tail=F))
+	else return(obs_n)
+}
+calc_p_v=Vectorize(calc_p)
+
+data.frame(thr=seq(0.1,1,0.05)) %>% mutate(p=calc_p_v(thr)) %>% mutate(adj=p.adjust(p, method="BH"))
+calc_p(0.75, full=T)
+
+pdf(paste0(BASEDIR, "/beta-actin-expected_freq.pdf"))
+calc_p(0.75, full=T) %>% 
+	ggplot(aes(x=paste(A650, A1322, A1533), y=n)) + 
+		geom_col() + 
+		geom_col(aes(y=exp), colour="blue", fill="blue", alpha=0.1 ,linetype="dotted") + 
+		theme_bw(22) + ylab("Number of molecules") + 
+		theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+dev.off()
