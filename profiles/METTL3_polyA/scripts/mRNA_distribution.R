e69be1e019010dcdf9edcc34e6038d628b0ae4f3
Tommaso Leonardi <tom@itm6.xyz>
2019-11-18 17:08:45 +0100

    Updated paths

--- profiles/METTL3_polyA/scripts/mRNA_distribution.R
+++ profiles/METTL3_polyA/scripts/mRNA_distribution.R
@@ -6,8 +6,8 @@ library(cowplot)
 library(lemon)
 library(ggrepel)
 txdb <- TxDb.Hsapiens.UCSC.hg38.knownGene
-
-f="/home/tleonardi/programming/bioinformatics/nanocompore_paper_analyses/nanocompore_pipelines/METTL3_KD_polyA/results/nanocompore/out_sig_sites_GMM_logit_pvalue_thr_0.05.bed"
+ROOTDIR=system2("git", args=c("rev-parse", "--show-toplevel"), stdout=T)
+f=paste0(ROOTDIR, "/nanocompore_pipelines/METTL3_KD_polyA/results/nanocompore/out_sig_sites_GMM_logit_pvalue_thr_0.05.bed")
 ncmp_results_sel <- pipe(paste0("tail -n +2 ", f, "| bedparse convertChr --assembly hg38 --target ucsc")) %>% read_tsv(, col_names=c("chr", "start", "end", "name", "score", "strand"))
 # Use all differential sites
 
@@ -19,7 +19,7 @@ diffSites <- list(sites001)
 names(diffSites) <- c("p-value<0.01")
 
 # Generate guitar plot
-pdf("/home/tleonardi/programming/bioinformatics/nanocompore_paper_analyses/profiles/METTL3_polyA/results/mRNA_profile.pdf")
+pdf(paste0(ROOTDIR,"/profiles/METTL3_polyA/results/mRNA_profile.pdf"))
 GuitarPlot(stGRangeLists = diffSites, txTxdb=txdb, pltTxType=c("mrna"), headOrtail=FALSE, enableCI=FALSE)
 dev.off()
 
