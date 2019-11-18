7bcfe0968a897702d5746d8dcef997b6afa941e5
Tommaso Leonardi <tom@itm6.xyz>
2019-11-18 16:37:54 +0100

    Updated BASEDIR

--- generic_seq_metrics/scripts/metrics_METTL3_KD_polyA.R
+++ generic_seq_metrics/scripts/metrics_METTL3_KD_polyA.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(UpSetR)
 
-BASEDIR="/home/tleonardi/programming/bioinformatics/nanocompore_paper_analyses"
+BASEDIR=$(git rev-parse --show-toplevel)
 
 counts <- read_tsv(paste0(BASEDIR, "/generic_seq_metrics/results/METTL3_KD_polyA/tx_counts.txt"), col_names=c("Sample", "n_reads", "Tx")) %>%
 	mutate(Sample=factor(Sample, levels=c("WT_1", "WT_2", "KD_1", "KD_2")))
